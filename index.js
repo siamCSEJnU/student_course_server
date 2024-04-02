bae33f0d042729f65879433e1d992d589b692e76
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2024-04-02 21:24:29 +0600

    delete and get of individual student api created

--- index.js
+++ index.js
@@ -15,19 +15,95 @@ const db = mysql.createConnection({
   database: "student_course_db",
 });
 
+//get api
 app.get("/students", (req, res) => {
   const query = `
-    SELECT s.Name AS StudentName
-    c.Name AS ClassName
-    s.DOB
-    s.Gender
-
-    FROM student s
-    INNER JOIN class c ON s.ClassId = c.Id
+    SELECT 
+        s.Id,
+        s.Name AS StudentName,
+        c.Name AS ClassName,
+        s.DOB,
+        s.Gender
+        
+    FROM 
+        student s
+    INNER JOIN 
+        class c ON s.ClassId = c.Id
     `;
   db.query(query, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
+  });
+});
+
+app.get("/students/:Id", (req, res) => {
+  const { Id } = req.params;
+  const query = `
+    SELECT 
+        s.Id,
+        s.Name,
+        s.ClassId,
+        s.DOB,
+        s.Gender,
+        s.CreatedDate,
+        s.ModificationDate
+    FROM 
+        student s
+    WHERE 
+        s.Id = ?
+    `;
+  db.query(query, [Id], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data[0]);
+  });
+});
+
+//update api
+app.put("/edit/:Id", (req, res) => {
+  const { Id } = req.params;
+  const { name, classId, dob, gender } = req.body;
+
+  const modifiedDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const genderValue = gender === "male" ? 1 : 2;
+
+  const sql =
+    "UPDATE Student SET Name = ?, ClassId = ?, DOB = ?, Gender = ?,ModificationDate=? WHERE Id = ?";
+  db.query(
+    sql,
+    [name, classId, dob, genderValue, modifiedDate, Id],
+    (err, result) => {
+      if (err) return res.json(err);
+      return res.json("updated");
+    }
+  );
+});
+
+//post api
+app.post("/create", (req, res) => {
+  const { name, classId, dob, gender } = req.body;
+  // console.log(name, classId, dob, gender);
+  const createdDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const genderValue = gender === "male" ? 1 : 2;
+  const sql =
+    "INSERT INTO Student (Name, Gender, DOB, ClassId, CreatedDate) VALUES (?, ?, ?, ?, ?)";
+  db.query(
+    sql,
+    [name, genderValue, dob, classId, createdDate],
+    (err, result) => {
+      if (err) return res.json(err);
+      return res.json("created");
+    }
+  );
+});
+
+//delete api
+app.delete("/students/:Id", (req, res) => {
+  const { Id } = req.params;
+
+  const sql = "DELETE FROM Student WHERE Id = ?";
+  db.query(sql, [Id], (err, result) => {
+    if (err) return res.json(err);
+    return res.json("deleted");
   });
 });
 
